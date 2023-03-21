c35db5ee163b7af88c8801fc842a9a12793b3bdd
Gustavo Sorati <sorati.gustavo@gmail.com>
2023-03-21 14:35:41 -0300

    feat: create navabar component

---
+++ src/components/Navbar/index.tsx
@@ -0,0 +1,44 @@
+import * as Styled from "./styles";
+
+import logoSvg from '../../aseets/logo.svg'
+import Image from "next/image";
+import Link from "next/link";
+import { ChartLineUp, Binoculars, SignIn } from "@phosphor-icons/react";
+import { useRouter } from "next/router";
+import { theme } from '@/styles/stitches.config';
+
+export function Navbar() {
+  const router = useRouter()
+  const { colors } = theme
+  
+  return (
+    <Styled.Container>
+      <Link href="/home" className="logo">
+        <Image
+          src={logoSvg}
+          alt=""
+          width={128}
+        />
+      </Link>
+
+      <Styled.Link href="/home"
+        active={router.pathname.includes('/home')}  
+      >
+        <ChartLineUp size={24} />
+        Início
+      </Styled.Link>
+
+      <Styled.Link href="/explore" 
+        active={router.pathname.includes('/explore')}
+      >
+        <Binoculars size={24} />
+        Explorar
+      </Styled.Link>
+
+      <Styled.SignIn>
+        Fazer Login
+        <SignIn size={20} color={colors.green100.value} />
+      </Styled.SignIn>
+    </Styled.Container>
+  )
+}
