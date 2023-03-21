9e933c630656fe2eda50af56e11c27798ea76b10
Gustavo Sorati <sorati.gustavo@gmail.com>
2023-03-21 13:38:57 -0300

    feat: create signinbutton component

---
+++ src/components/SignInButton/index.tsx
@@ -0,0 +1,25 @@
+import Image from "next/image";
+import { ComponentProps } from "react";
+
+import GoogleSvg from '../../aseets/icons/google.svg';
+import * as Styled from "./styles";
+
+type Props = ComponentProps<typeof Styled.Container> & {
+  text: string
+  imgsrc: string
+}
+
+export function SignInButton({ text, imgsrc, ...rest}: Props) {
+  return (
+    <Styled.Container {...rest}>
+      <Image
+        src={imgsrc}
+        alt="google"
+        width={32}
+        height={32}
+      />
+
+      Entrar com {text}
+    </Styled.Container>
+  )
+}
