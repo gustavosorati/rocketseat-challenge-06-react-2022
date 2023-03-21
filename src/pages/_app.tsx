683c9d1623e3868cb2c9ae0204af8002bff26a5e
Gustavo Sorati <sorati.gustavo@gmail.com>
2023-03-21 10:22:11 -0300

    chore: add google nunito font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,14 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { Nunito } from 'next/font/google';
+
+const nunito = Nunito({ 
+  weight: ['400', '700'],
+  subsets: ['latin']
+})
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <main className={`${nunito.className}`}>
+    <Component {...pageProps} />
+  </main>
 }
