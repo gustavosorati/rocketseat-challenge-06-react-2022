5e75e061b3c1575a2778f033e05c372c7b0a60b1
Gustavo Sorati <sorati.gustavo@gmail.com>
2023-03-22 12:41:19 -0300

    feat: create home page

--- src/pages/home/index.page.tsx
+++ src/pages/home/index.page.tsx
@@ -3,8 +3,15 @@ import Head from 'next/head'
 import * as Styled from './styles'
 
 import { Navbar } from '@/components/Navbar'
+import { theme } from '@/styles/stitches.config';
+import { ChartLine } from '@phosphor-icons/react';
+import { Card } from '@/components/Card';
+import Link from 'next/link';
+import { ShyCard } from '@/components/Book/ShyCard';
 
 export default function Home() {
+  const { colors } = theme
+
   return (
     <>
       <Head>
@@ -18,10 +25,37 @@ export default function Home() {
         <Navbar />
 
         <Styled.Content>
-          fdsfsdf
+          <div className="header">
+            <ChartLine size={32} color={colors.green100.value} />
+            <h2>Início</h2>
+          </div>
 
-          
+          <div className='sections'>
+            <Styled.BooksContent>
+              <h6>Avaliações mais recentes</h6>
+
+              <Card />
+              <Card />
+              <Card />
+            </Styled.BooksContent>
+
+            <Styled.Aside>
+              <div className="header">
+                <h5>Livros populares</h5>
+
+                <Link href="#">Ver todos</Link>
+              </div>
+
+              <ShyCard />
+
+              <ShyCard />
+              
+              <ShyCard />
+            </Styled.Aside>
+          </div>
         </Styled.Content>
+
+        
       </Styled.Container>
     </>
   )
