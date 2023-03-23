63662b2eb2e285346f21e3affb3e940cdad417aa
Gustavo Sorati <sorati.gustavo@gmail.com>
2023-03-23 12:46:15 -0300

    feate: create profile page

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  pageExtensions: [ 'page.tsx', 'api.ts' ]
+  pageExtensions: [ 'page.tsx', 'api.ts' ],
+  images: {
+    domains: ['avatars.githubusercontent.com']
+  }
 }
 
 module.exports = nextConfig
